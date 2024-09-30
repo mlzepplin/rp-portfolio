e0cee8f57d897e284bda4f3a41988a33babf67b7
mlzepplin <rishabh.gupta.1606@gmail.com>
2024-09-30 00:22:03 -0700

    add project views without the templates

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+# projects/views.py
 
-# Create your views here.
+from django.shortcuts import render
+from projects.models import Project
+
+def project_index(request):
+    # Perform a retrieve query on our database.
+    projects = Project.objects.all()
+    # Define a dictionary context with only one KV pair where value is all the projects we retrieved.
+    context = {
+        "projects": projects
+    }
+    # Passing the context dictionary as an argument to the template.
+    return render(request, "projects/project_index.html", context)
+
+def project_detail(request, pk):
+    # Perform a retrieve query which retrieves the project with primary key, pk, equal to functions argument.
+    project = Project.objects.get(pk=pk)
+    context = {
+        "project": project
+    }
+    return render(request, "projects/project_detail.html", context)
